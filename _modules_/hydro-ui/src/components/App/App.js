056610f772ae23ab73b4c3c067755b804414444c
JeffTheNinja57 <mihny.angheluta@gmail.com>
2023-10-10 22:18:52 +0200

    move the files around, edit .gitignore, add @mui/modules

---
+++ _modules_/hydro-ui/src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainPage from '../Dashboard/MainPage';
+import NavBar from "../NavBar/NavBar";
+
+function App() {
+  return (
+      <div>
+          <NavBar />
+      </div>
+
+  );
+}
+
+export default App;
